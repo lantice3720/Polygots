d7c61b2b72dc6649ce94a416d8ed409da633f1dd
lantice3720 <65650884+lantice3720@users.noreply.github.com>
2023-09-28 01:25:21 +0900

    초기 커밋.

---
+++ PolygotForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Polygots.Properties;
+
+namespace Polygots
+{
+    partial class PolygotForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void texture_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(Resources.RectPng, 0, 0, this.Width, this.Height);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            var Random = new Random();
+            this.Location = new Point(Random.Next(0, Screen.PrimaryScreen.Bounds.Width - this.Width), Random.Next(0, Screen.PrimaryScreen.Bounds.Height - this.Height));
+            this.Size = new Size(64, 64);
+            this.texture.Paint += texture_Paint;
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            this.Size = new Size(96, 96);
+            Console.WriteLine("Mouse Down");
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.texture = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // texture
+            // 
+            this.texture.BackColor = System.Drawing.Color.Transparent;
+            this.texture.Location = new System.Drawing.Point(0, 0);
+            this.texture.Name = "texture";
+            this.texture.Size = new System.Drawing.Size(64, 64);
+            this.texture.TabIndex = 0;
+            // 
+            // PolygotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(64, 64);
+            this.Controls.Add(this.texture);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PolygotForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Polygot";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel texture;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Polygots.Properties;
+
+namespace Polygots
+{
+    internal static class Program
+    {
+        public static List<DialogResult> Dialogs = new List<DialogResult>();
+
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Console.WriteLine("Initializeing Polygots v{0}", Resources.version);
+
+            using (var tray = new NotifyIcon())
+            {
+                tray.Text = "Polygots";
+                tray.Icon = Resources.Rect;
+                tray.Visible = true;
+
+                tray.ContextMenuStrip = new ContextMenuStrip();
+                tray.ContextMenuStrip.Items.Add("About", null, (sender, e) => MessageBox.Show($"Polygots v{Resources.version}", "Info", MessageBoxButtons.OK, MessageBoxIcon.None));
+                tray.ContextMenuStrip.Items.Add("Summon", null, TrayMenu_Summon);
+                tray.ContextMenuStrip.Items.Add("Exit", null, TrayMenu_Exit);
+
+                Application.Run();
+            }
+            // Application.Run(new Form1());
+        }
+
+        private static void TrayMenu_Summon(object sender, EventArgs e)
+        {
+            new PolygotForm().Show();
+        }
+
+        private static void TrayMenu_Exit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
